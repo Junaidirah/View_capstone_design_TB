d733a56d6a68e01aa49e9adb0ed1f43029d2c89c
junaidirah <junaidirahmat02@gmail.com>
2025-06-01 20:42:40 +0700

    fix browser route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import Auth from './components/Auth';
 import SignUp from './components/SignUp'; 
